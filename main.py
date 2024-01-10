0e167f84a15dc405674d5c0431f7096510f4c9a4
Haisenberg <1paf1@ukr.net>
2024-01-10 22:02:26 +0200

    tasks

---
+++ main.py
@@ -0,0 +1,54 @@
+# 1.Користувач вводить рядок з клавіатури. Порахуйте кількість літер, цифр у рядку. Виведіть обидві кількості на екран.
+# (використовувати цикл)?
+
+
+
+# letter_counter = 0
+# digit_counter = 0
+# row = input("Enter row: ")
+# for letter in row:
+#     if letter.isalpha():
+#         letter_counter += 1
+#     elif letter.isdigit():
+#         digit_counter += 1
+# print(f"Letters: {letter_counter}")
+# print(f"Digits: {digit_counter}")
+
+
+
+
+# 2.Користувач вводить з клавіатури рядок та символ для пошуку. Порахуйте скільки разів у рядку зустрічається потрібний символ.
+# Отримане число виведіть на екран.
+
+# total = 0
+# row = input("Enter a text: ")
+# symbol_to_find = input("Enter the symbol: ")
+# for symbol in row:
+#     if symbol == symbol_to_find:
+#         total += 1
+# print(total)
+
+# 3.Користувач вводить з клавіатури рядок, слово для пошуку, слово для заміни. Зробіть у рядку заміну одного слова на інше.
+# Виведіть отриманий рядок на екрані.
+
+# input_string = input("Enter string: ")
+# word_to_find = input("Enter word to find: ")
+# replacement_word = input("Enter word to replace the word you selected: ")
+# final_string = input_string.replace(word_to_find, replacement_word)
+#
+# print(f"final string: {final_string}")
+
+
+
+# task 4
+# string = input('Enter a string: ')
+#
+# print(string[3])
+# print(string[-2])
+# print(string[:5])
+# print(string[:-2])
+# print(string[::2])
+# print(string[1::2])
+# print(string[::-1])
+# print(string[-1::-2])
+# print(len(string))
